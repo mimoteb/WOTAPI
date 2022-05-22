ccd6879608f878d711cd9524760389ee2ea58757
Solomon Samuel <10999718+mimoteb@users.noreply.github.com>
2022-05-22 16:58:20 +0200

    wot api

---
+++ Functions.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Xml
+Imports Newtonsoft.Json
+
+Module Functions
+    Public front_id As String = "renaissance_bg"
+    Public event_id As String = "renaissance"
+    Public api_id As String
+    Public api_lang As String
+    Public status As String
+    Public count As String
+
+    Public Function Json2XML(ByVal json As String) As XmlDocument
+        Return JsonConvert.DeserializeXmlNode(json, "Root")
+    End Function
+    Public Sub ErrorLog(ByVal sender As String, errorMsg As String)
+
+    End Sub
+End Module
